7b4481a9e165a2f28dcabba0ba3081f852d29e3c
JumabaevPolatbek <mouz.cyx@mail.ru>
2023-01-12 00:37:39 +0500

    url?

--- src/store/api/product/index.ts
+++ src/store/api/product/index.ts
@@ -5,7 +5,7 @@ import { pathApi } from '..';
 
 export const actionsProduct = createApi({
     reducerPath: 'actionsProduct',
-    baseQuery: fetchBaseQuery({ baseUrl: pathApi }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://ecommerce-h6sh.onrender.com/' }),
     endpoints: (builder) => ({
         getProducts: builder.query<Products,void>({
             query: () => `products/`,
