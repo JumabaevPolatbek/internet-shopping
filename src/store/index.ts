3d5536b020ed97ae0d7c4234d6767b53ec6c35b4
JumabaevPolatbek <mouz.cyx@mail.ru>
2023-01-12 11:29:52 +0500

    categories?

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { rootReducer } from "./reducer";
 import { actionsCountrie } from "./api/countrie";
-import { actionsCategory } from "./api/category";
+import { actionsCategories } from "./api/category";
 import { actionsProduct } from "./api/product";
 import { actionsUser } from "./api/user";
 // import logger from 'redux-logger'
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(actionsCountrie.middleware, actionsCategory.middleware, actionsProduct.middleware, actionsUser.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        actionsCountrie.middleware,
+        actionsCategories.middleware,
+        actionsProduct.middleware,
+        actionsUser.middleware)
 });
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
